fa0fc0f5794fadd668f99a08ec9ad31bceb9633f
Sakshi <sakshisachin37@gmail.com>
2026-04-15 15:13:17 +0530

    Initial commit

---
+++ src/containers/greeting/Greeting.js
@@ -0,0 +1,87 @@
+import React, { useContext } from "react";
+import { Fade } from "react-reveal";
+import emoji from "react-easy-emoji";
+import "./Greeting.scss";
+import landingPerson from "../../assets/lottie/landingPerson";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import SocialMedia from "../../components/socialMedia/SocialMedia";
+import Button from "../../components/button/Button";
+import { illustration, greeting } from "../../portfolio";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Greeting() {
+  const { isDark } = useContext(StyleContext);
+  if (!greeting.displayGreeting) {
+    return null;
+  }
+  return (
+    <Fade bottom duration={1000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+
+          {/* 🔥 LEFT SIDE (TEXT + PROFILE IMAGE) */}
+          <div className="greeting-text-div">
+            <div>
+
+              {/* ✅ Profile Image */}
+              <div className="profile-image-div">
+                <img
+                  src={require("../../assets/images/lo1.jpeg")}
+                  alt="profile"
+                  className="profile-image"
+                />
+              </div>
+
+              <h1
+                className={isDark ? "dark-mode greeting-text" : "greeting-text"}
+              >
+                {greeting.title}{" "}
+                <span className="wave-emoji">{emoji("👋")}</span>
+              </h1>
+
+              <p
+                className={
+                  isDark
+                    ? "dark-mode greeting-text-p"
+                    : "greeting-text-p subTitle"
+                }
+              >
+                {greeting.subTitle}
+              </p>
+
+              <div id="resume" className="empty-div"></div>
+
+              <SocialMedia />
+
+              <div className="button-greeting-div">
+                <Button text="Contact me" href="#contact" />
+                {greeting.resumeLink && (
+                  <a
+                    href={require("./resume.pdf")}
+                    download="Resume.pdf"
+                    className="download-link-button"
+                  >
+                    <Button text="Download my resume" />
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+
+          {/* RIGHT SIDE (Animation) */}
+          <div className="greeting-image-div">
+            {illustration.animated ? (
+              <DisplayLottie animationData={landingPerson} />
+            ) : (
+              <img
+                alt="man sitting on table"
+                src={require("../../assets/images/manOnTable.svg")}
+              ></img>
+            )}
+          </div>
+
+        </div>
+      </div>
+    </Fade>
+  );
+}
---
+++ src/portfolio.js
@@ -0,0 +1,342 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Lochana pandith B R",
+  title: "Hi all, I'm Lochana",
+  subTitle: emoji(
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/19Hk93TpDyKPLSTMzIIHsmKBlhGNLNueM/view?usp=drivesdk", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+  
+};
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/lochulochana77-max",
+  linkedin: "https://www.linkedin.com/in/lochana-pandith-b-r-556811395?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  gmail: "lochulochana77@gmai.com",
+  facebook: "https://www.instagram.com/__.uniquesmiler.__?utm_source=qr&igsh=MXRleTI2Nnh3MzY0eg==",
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "I am a student dedicated to learning, exploring new ideas, and building my skills. This portfolio highlights my journey, projects, and interests.",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "marimallapa pu college mysuru",
+      logo: require("./assets/images/mari2.jpeg"),
+      subHeader: "PCMB",
+      duration: "2020- 2022",
+
+    },
+    {
+      schoolName: "Youraja college ,mysore",
+      logo: require("./assets/images/yuvalogo1.jpeg"),
+      subHeader: "Bachelor of Science in Computer Science",
+      duration: "2022 - 2025",
+      
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: false, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Software Engineer",
+      company: "Facebook",
+      companylogo: require("./assets/images/facebookLogo.png"),
+      date: "June 2018 – Present",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      descBullets: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      ]
+    },
+    {
+      role: "Front-End Developer",
+      company: "Quora",
+      companylogo: require("./assets/images/quoraLogo.png"),
+      date: "May 2017 – May 2018",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      role: "Software Engineer Intern",
+      company: "Airbnb",
+      companylogo: require("./assets/images/airbnbLogo.png"),
+      date: "Jan 2015 – Sep 2015",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/pro.jpeg"),
+      projectName: "improving basic sentiment analyzer",
+  
+    },
+    
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "customer engagement:problem solving and process controls",
+      image: require("./assets/images/problochu.jpeg"),
+      imageAlt: "customer engagement:problem solving and process controls",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://drive.google.com/file/d/1hAPq-b3_fmOVvyCTNRwWLjME5kELDcRd/view?usp=drivesdk"
+        },
+      ]
+    },
+    {
+      title: "artificial intelligence",
+      image: require("./assets/images/ailochu.jpeg"),
+      imageAlt: "artificial intelligence",
+      footerLink: [
+        {
+          name: "certification",
+          url: "https://drive.google.com/file/d/1V9yBgUzUKv74puFEaDQQxqLR_cO14r-Y/view?usp=drivesdk"
+        }
+      ]
+    },
+
+    {
+      title: "customer engagement:communication and personility dynamics",
+      image: require("./assets/images/comunlochu.jpeg"),
+      imageAlt: "customer engagement:communication and personility dynamics",
+      footerLink: [
+        {name: "Certification", url: "https://drive.google.com/file/d/1HCasn6IotCM57E16l1GkyQoBxO7rfo0J/view?usp=drivesdk"},
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+      
+    }
+  ],
+  display: false// Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: false// Set false to hide this section, defaults to true
+};
+
+// Resume Section
+const resumeSection = {
+  title: "Resume",
+  subtitle: "Feel free to download my resume",
+
+  // Please Provide with Your Podcast embeded Link
+  display: true // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+91-9449408423",
+  email_address: "lochulochana77@gmai.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: true // Set true to display this section, defaults to false
+};
+
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable,
+  resumeSection
+};
